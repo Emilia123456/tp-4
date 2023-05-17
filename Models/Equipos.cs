7c47bae91689d84bc1475aca7b897166e30c19dc
EMILIA INÉS ARMENO <47697893@ort>
2023-05-17 09:46:56 -0300

    Inicializamos clases

---
+++ Models/Equipos.cs
@@ -0,0 +1,11 @@
+static class Equipos{
+    List<string> ListaEquipos {get; private set;}
+    List<string> ListaMedias {get; private set;}
+    List<string> ListaPantalones {get; private set;}
+    List<string> ListaRemeras {get; private set;}
+    Dictionary<string, indumentaria> EquiposIndumentaria {get; private set;}
+    //metodos
+    public bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria item){
+        
+    }
+}
---
+++ Models/Indumentaria.cs
@@ -0,0 +1,17 @@
+class Indumentaria {
+    //propiedades
+    public string Medias {get; private set;}
+    public string Pantalon {get; private set;}
+    public string Camiseta {get; private set;}
+    // constructor vacio
+    public Indumentaria(){
+    }
+    //constructor
+    public indumentaria(string Medias, string Pantalon, string Camiseta){
+        Medias=medias;
+        Pantalon=pantalon; 
+        Camiseta=camiseta; 
+    }
+    
+
+}
