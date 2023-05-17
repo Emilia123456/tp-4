58f752bcb4e89d99bf97648a5d4b5daa35d3fa81
EMILIA INÉS ARMENO <47697893@ort>
2023-05-17 10:24:40 -0300

    uwu

--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -1,11 +1,14 @@
 static class Equipos{
-    List<string> ListaEquipos {get; private set;}
+    List<string> ListaEquipos {get; private set;} = new List<string>("Sacachispas", "Deportivo Moron", "Villa Mitre", "San Lorenzo", "River", "Racing", "Chacarita","Independiente","Atlanta","Velez" );
     List<string> ListaMedias {get; private set;}
     List<string> ListaPantalones {get; private set;}
     List<string> ListaRemeras {get; private set;}
-    Dictionary<string, indumentaria> EquiposIndumentaria {get; private set;}
+    Dictionary<string, indumentaria> EquiposIndumentaria {get; private set;} = new Dictionary <string, Indumentaria>();
     //metodos
     public bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria item){
-        
+        if(EquiposIn)
+        dicEquiposIndumentaria.Add(EquipoSeleccionado, item);
+
     }
+   
 }
