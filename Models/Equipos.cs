df278c2bc4045ce1ad64b377cc3d4580ab1603f6
EMILIA INÉS ARMENO <47697893@ort>
2023-05-18 15:00:38 -0300

    otra vez

--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -1,8 +1,8 @@
 static class Equipos{
-   public static List<string> ListaEquipos {get; private set;} = new List<string>(){"Sacachispas", "Deportivo Moron", "Olimpo", "San Lorenzo", "River", "Racing", "Chacarita","Independiente","Atlanta","Velez" };
-   public static List<string> ListaMedias {get; private set;} = new 
-   public static List<string> ListaPantalones {get; private set;}
-   public static List<string> ListaRemeras {get; private set;}
+   public static List<string> ListaEquipos {get; private set;} = new List<string>(){"sacachispas", "moron", "olimpo", "sanLorenzo", "river", "racing", "chacarita","independiente","atlanta","velez" };
+   public static List<string> ListaMedias {get; private set;} = new List<string>(){"sacachispas", "moron", "olimpo", "sanLorenzo", "river", "racing", "chacarita","independiente","atlanta","velez" };
+   public static List<string> ListaPantalones {get; private set;}= new List<string>(){"sacachispas", "moron", "olimpo", "sanLorenzo", "river", "racing", "chacarita","independiente","atlanta","velez" };
+   public static List<string> ListaRemeras {get; private set;}= new List<string>(){"sacachispas", "moron", "olimpo", "sanLorenzo", "river", "racing", "chacarita","independiente","atlanta","velez" };
    public static Dictionary<string, Indumentaria> EquiposIndumentaria {get; private set;} = new Dictionary <string, Indumentaria>();
     //metodos
     public static bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria item){
